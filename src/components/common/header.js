7192386a3a44cb195d10c9f7264153e9c5c79cf6
“ <mayurjikar7771@gmail.com>
2021-05-02 14:26:56 +0530

    responsiv webite

---
+++ src/components/common/header.js
@@ -0,0 +1,71 @@
+import React,{ useState }from 'react';
+//import { Menu } from 'antd';
+import { Anchor,Drawer, Button } from 'antd';
+
+const { Link } = Anchor;
+const AppHeader = () => {
+    const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+   
+     return (
+          
+        <div className="container-fluid">
+            <div className="header">
+            <div className="logo">
+                 <i className="fas fa-bolt"></i>
+                 <a href="http://google.com">Tech</a>
+            </div>
+            {/* <Menu  mode="horizontal" defaultSelectedKeys={['home']}>
+                <Menu.Item key="home">Home</Menu.Item>
+                <Menu.Item key="about">About</Menu.Item>
+                <Menu.Item key="features">Features</Menu.Item>
+                <Menu.Item key="howitwork">How it work</Menu.Item>
+                <Menu.Item key="faq">FAQ</Menu.Item>
+                <Menu.Item key="pricing">Pricing</Menu.Item>
+                <Menu.Item key="contact">Contact</Menu.Item>
+            </Menu> */}
+             <div className="mobileHidden">
+                 <Anchor targetOffset="65"> 
+                    <Link href="#hero" title="Home" />
+                    <Link href="#about" title="About" />
+                    <Link href="#feature" title="Features" />
+                    <Link href="#works" title="How it works" />
+                    <Link href="#faq" title="FAQ" />
+                    <Link href="#pricing" title="Pricing" />
+                    <Link href="#contact" title="Contact" />
+                  </Anchor>
+               </div>
+               <div className="mobileVisible">
+               <Button type="primary" onClick={showDrawer}>
+              <i className="fas fa-bars"></i>
+              </Button>
+              <Drawer
+                      placement="right"
+                      mask={false}
+                     onClose={onClose}
+                     visible={visible}
+               >
+                 <Anchor targetOffset="65"> 
+                    <Link href="#hero" title="Home" />
+                    <Link href="#about" title="About" />
+                    <Link href="#feature" title="Features" />
+                    <Link href="#works" title="How it works" />
+                    <Link href="#faq" title="FAQ" />
+                    <Link href="#pricing" title="Pricing" />
+                    <Link href="#contact" title="Contact" />
+                  </Anchor>
+             </Drawer>
+               </div>
+            </div>
+        </div>
+
+     );
+
+}
+
+export default AppHeader;
